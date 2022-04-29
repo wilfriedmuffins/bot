24f8b68feaa3755b57843eebf30651d8dd4d8fc5
wilfriedmuffins <wilfried.moffen@live.fr>
2022-04-29 18:10:46 +0200

    init commit: config bot

---
+++ app/bot/message.rb
@@ -0,0 +1,12 @@
+require 'facebook/messenger'
+include Facebook::Messenger
+
+Facebook::Messenger::Subscriptions.subscribe(
+    access_token: ENV["ACCESS_TOKEN"],
+    subscribed_fields: %w[feed mention name]
+  )
+
+Faceboook::Messenger::Bot.on :message do |message|
+  message.reply(text: 'Hello, human!')
+end
+
